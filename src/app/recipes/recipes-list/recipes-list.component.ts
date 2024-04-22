4f4d36254f0dfb24f6834535ba2998a784c9ba35
Timofey <pttima@yandex.ru>
2024-04-22 15:34:45 +0300

    Added recipe in the list by clicking button

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = []
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addRecipeInTheList() {
+    this.recipes.push(new Recipe('Test Recipe', 'This is just a test', 'https://www.foodandwine.com/thmb/fVmYbaQzXCz1Prx8VxrW9sMcjMU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Braciole-FT-RECIPE1122-66acf49cef0e4390bec780945709e7f3.jpg'))
   }
 
 }
