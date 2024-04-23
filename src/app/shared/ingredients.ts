b339cf39b67317cd534f0183f2b23ba64d92ad64
Timofey <pttima@yandex.ru>
2024-04-23 13:02:49 +0300

    Basic overly work set. Shopping editor, shopping list etc

---
+++ src/app/shared/ingredients.ts
@@ -0,0 +1,4 @@
+
+export class Ingredients {
+   constructor(private name: string, private amount: number) {}
+}
--- src/app/shoping-list/shoping-list.component.ts
+++ src/app/shoping-list/shoping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredients } from '../shared/ingredients';
 
 @Component({
   selector: 'app-shoping-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shoping-list.component.css']
 })
 export class ShopingListComponent implements OnInit {
+  ingredients: Ingredients[] = [
+    new Ingredients('Apples', 10),
+    new Ingredients('Tomatos', 5)
+  ]
 
   constructor() { }
 
