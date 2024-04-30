de02c5418af00067ab71df0ad3ef64b2894e2b33
Timofey <pttima@yandex.ru>
2024-04-30 13:38:18 +0300

    Recipe details comp decompose and some new stuff added

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  @Output() recipeWasSelcted = new EventEmitter<Recipe>()
   recipes: Recipe[] = []
 
   constructor() { }
@@ -16,6 +17,10 @@ export class RecipesListComponent implements OnInit {
 
   addRecipeInTheList() {
     this.recipes.push(new Recipe('Test Recipe', 'This is just a test', 'https://www.foodandwine.com/thmb/fVmYbaQzXCz1Prx8VxrW9sMcjMU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Braciole-FT-RECIPE1122-66acf49cef0e4390bec780945709e7f3.jpg'))
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelcted.emit(recipe)
   }
 
 }
